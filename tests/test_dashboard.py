2bd993c751fe57e64746197d3c1f27f1a4dd6e83
Keenan Johnson <keenan.johnson@gmail.com>
2023-01-25 19:15:56 -0800

    Updated URL of dashboard page

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -1,7 +1,7 @@
 import requests 
 
 def test_ribbit_dashboard():
-    r = requests.get("https://ribbit-network.herokuapp.com/")
+    r = requests.get("https://dashboard.ribbitnetwork.org/")
     if r.status_code == 200:
         assert True 
     else:
